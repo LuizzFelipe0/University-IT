5de0006a55b1472d063adb9db5bbd3bf90dd066e
Luiz Felipe Canário Costa <luizfelipecanariocosta@gmail.com>
2022-10-05 18:30:29 -0300

    2455

---
+++ C/gangorra.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+
+int p1,p2,c1,c2,operation;
+
+scanf("%d", &p1);
+scanf("%d", &c1);
+scanf("%d", &p2);
+scanf("%d", &c2);
+
+if ((p1*c1)==(p2*c2)){
+    printf("0\n");
+}else if ((p1*c1)>(p2*c2)){
+    printf("-1\n");
+}else{
+    printf("1\n");
+}
+
+
+    return 0;
+}
