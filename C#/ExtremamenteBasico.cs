99a0a71e5f69934469dd6ef7ed39faabd0c2e6e0
Luiz Felipe Canário Costa <luizfelipecanariocosta@gmail.com>
2022-11-14 21:07:34 -0300

    C#

---
+++ C#/ExtremamenteBasico.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+
+    public static void Main(string[] args)
+    {
+
+        int n1 = Convert.ToInt32(Console.ReadLine());
+        int n2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("X = {0}", n1 + n2);
+
+
+    }
+}
